ec096eeba91d92431f0d19f6fe5b6bbf4a02cb88
Haroon Khan <haroon.khan.am@gmail.com>
2025-08-04 12:01:01 +0500

    Your message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { HeroComponent } from './component/hero/hero.component';
+import { FeatureComponent } from "./component/feature/feature.component";
+import { SkillComponent } from "./component/skill/skill.component";
+import { ExperienceComponent } from "./component/experience/experience.component";
+import { ProjectComponent } from "./component/project/project.component";
+import { NavbarComponent } from "./component/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeroComponent, FeatureComponent, SkillComponent, ExperienceComponent, ProjectComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'demo';
